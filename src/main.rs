33732b4e426abc7f064a54e8c61977c4975963f9
Seamus <m0ose01@proton.me>
2024-09-27 00:09:41 +0100

    Add command line options for input and output.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    input: String,
+    #[arg(short = 'o', long = "output", default_value_t = ("template.md").to_string())]
+    output: String,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{}", args.input);
+    println!("{}", args.output);
 }
