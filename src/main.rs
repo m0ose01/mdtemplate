15601b1e50e64d6748a6a5fd3398b252b7b49091
Seamus <m0ose01@proton.me>
2024-09-27 00:22:28 +0100

    Add more arguments.
    Add flags specifying whether or not to create template/slide images.
    Add help text to arguments.

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,14 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
+    #[arg(help = "Input file (must be a PDF).")]
     input: String,
-    #[arg(short = 'o', long = "output", default_value_t = ("template.md").to_string())]
+    #[arg(short = 'o', long = "output", default_value_t = ("template.md").to_string(), help = "Output file.")]
     output: String,
+    #[arg(long = "md", help = "Create markdown template.")]
+    create_markdown: bool,
+    #[arg(long = "img", help = "Create images from PDF.")]
+    create_images: bool,
 }
 
 
