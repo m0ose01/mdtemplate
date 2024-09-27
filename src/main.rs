c16f435ba15f06f916d7622f340694788b6574cc
Seamus <m0ose01@proton.me>
2024-09-27 01:14:31 +0100

    Add options.
    Add verbose output option.
    Only create images if option is specified.

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,21 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
+
     #[arg(help = "Input file (must be a PDF).")]
     input: String,
+
     #[arg(short = 'o', long = "output", default_value_t = ("template.md").to_string(), help = "Output file.")]
     output: String,
+
     #[arg(long = "md", help = "Create markdown template.")]
     create_markdown: bool,
+
     #[arg(long = "img", help = "Create images from PDF.")]
     create_images: bool,
+
+    #[arg(short = 'v', long = "verbose", help = "Verbose output.")]
+    verbose: bool,
 }
 
 
@@ -22,15 +29,22 @@ fn main() {
 
     let _ = fs::create_dir("slides");
 
-    let magick = Command::new("magick")
-        .arg(args.input)
-        .args(["-quality", "100"])
-        .arg("slides/img_%d.jpg")
-        .output()
-        .expect("Failed to launch imagemagick process.");
+    if args.create_images {
+        let magick = Command::new("magick")
+            .arg(args.input)
+            .args(["-quality", "100"])
+            .arg("slides/img_%d.jpg")
+            .output()
+            .expect("Failed to launch imagemagick process.");
 
-    match magick.status.code() {
-        Some(code) => println!("Magick exited with code {code}."),
-        None => println!("Magick process was terminated by a signal."),
+        match magick.status.code() {
+            Some(code) => println!("Magick exited with code {code}."),
+            None => println!("Magick process was terminated by a signal."),
+        }
+
+        if args.verbose {
+            println!("magick stdout: {:?}", magick.stdout);
+            println!("magick stderr: {:?}", magick.stderr);
+        }
     }
 }
